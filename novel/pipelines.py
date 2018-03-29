69d0dd27fa6fc3c0b749d19bf3d3e68e015772b6
RoachOwO <jack092123@gmail.com>
2018-03-29 10:17:05 +0800

    Using pipeline to save the result into a text file

--- novel/pipelines.py
+++ novel/pipelines.py
@@ -5,6 +5,16 @@
 # Don't forget to add your pipeline to the ITEM_PIPELINES setting
 # See: https://doc.scrapy.org/en/latest/topics/item-pipeline.html
 
+import json
+
 class NovelPipeline(object):    
+    def __init__(self):
+        self.file = open('novel.txt', 'w', encoding='utf-8')
+
     def process_item(self, item, spider):
+        line = json.dumps(dict(item), ensure_ascii=False) + "\n"
+        self.file.write(line)
         return item
+
+    def spider_closed(self, spider):
+        self.file.close()
